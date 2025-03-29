19f3ed4ec9ccf97d55177843d07bca5b0700074b
darkmalk <sa.vergaramunoz@gmail.com>
2025-03-29 06:12:41 -0300

    feat: Refactor loan state update logic and remove unused fine services

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,6 @@
-import { RouterUser, RouterRole, RouterBook, RouterGenre, RouterAuthor, RouterLoan, RouterFine } from './routes'
-import { updateLoanStateToExpired } from './services/loan/update_loan_expired'
-import { postUserFine } from './services/fine/post_user_fine'
+import { RouterUser, RouterRole, RouterBook, RouterGenre, RouterAuthor, RouterLoan } from './routes'
+import { updateLoanState } from './services/loan/update_loan_state'
 import { getAllLoans } from './services/loan/get_all_loans'
-import { newFine } from './services/fine/post_new_fine'
 import { notFound } from './middlewares'
 import express from 'express'
 import cron from 'node-cron'
@@ -23,12 +21,11 @@ app.use('/api/book', RouterBook)
 app.use('/api/genre', RouterGenre)
 app.use('/api/author', RouterAuthor)
 app.use('/api/loan', RouterLoan)
-app.use('/api/fine', RouterFine)
 
 // Schedule Cron for create fine
 cron.schedule('0 0 * * *', async () => {
   const allLoans = await getAllLoans()
-  const nowDate = new Date()
+  const nowDate = new Date().toISOString()
 
   if (!allLoans.length) return
 
@@ -36,9 +33,7 @@ cron.schedule('0 0 * * *', async () => {
     allLoans.map(async loan => {
       if (loan.state !== 'active') return true
       if (nowDate > loan.finish_date) {
-        await updateLoanStateToExpired(loan.id)
-        const { insertId } = await newFine({ value: 3000, state: 'pending' })
-        await postUserFine(loan.username, insertId)
+        updateLoanState(loan.id, 'expired')
 
         // TODO: Send email to user
 
