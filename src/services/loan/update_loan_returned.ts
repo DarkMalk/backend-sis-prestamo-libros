48fdacb46d3a58f45faa3cb42aa2995eaaee1014
darkmalk <sa.vergaramunoz@gmail.com>
2025-03-29 04:37:10 -0300

    refactor: Remove loan update controllers and related query utilities

--- src/services/loan/update_loan_returned.ts
+++
@@ -1,9 +0,0 @@
-import { ResultSetHeader } from 'mysql2'
-import { conn } from '../../config/db'
-import { query } from '../../utils/querys'
-
-export const updateLoanStateToReturned = async (id: number) => {
-  const [result] = await conn.query<ResultSetHeader>(query.updateLoanStateToReturned, [id])
-
-  return result
-}
