446ff6a1bad7bd68fee2afdd81b4c4a77abbfa9a
darkmalk <sa.vergaramunoz@gmail.com>
2025-03-29 02:11:57 -0300

    refactor: Remove fine route and related exports from the application

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,6 +4,5 @@ import RouterRole from './role'
 import RouterBook from './book'
 import { RouterAuthor } from './author'
 import { RouterLoan } from './loan'
-import { RouterFine } from './fine'
 
-export { RouterUser, RouterRole, RouterGenre, RouterBook, RouterAuthor, RouterLoan, RouterFine }
+export { RouterUser, RouterRole, RouterGenre, RouterBook, RouterAuthor, RouterLoan }
