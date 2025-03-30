e681f500fc6bc1272f8afdb5c6ae475b3e2fea92
darkmalk <sa.vergaramunoz@gmail.com>
2025-03-29 22:58:14 -0300

    feat: Add query to get book by name and update book stock functionality

--- src/utils/book/querys.ts
+++ src/utils/book/querys.ts
@@ -1,5 +1,7 @@
 export const query = {
   getAllBooks: `SELECT b.id, b.name, g.name as genre, a.name as author, b.isbn, e.name as editorial, b.stock FROM book b INNER JOIN genre g ON g.id = b.genre INNER JOIN author a ON a.id = b.author INNER JOIN editorial e ON e.id = b.editorial`,
   getOneBookById: `SELECT b.id, b.name, g.name as genre, a.name as author, b.isbn, e.name as editorial, b.stock FROM book b INNER JOIN genre g ON g.id = b.genre INNER JOIN author a ON a.id = b.author INNER JOIN editorial e ON e.id = b.editorial WHERE b.id = ?`,
-  postNewBook: 'INSERT INTO book (name, genre, author, isbn, editorial, stock) VALUES (?, ?, ?, ?, ?, ?)'
+  getOneBookByName: `SELECT b.id, b.name, g.name as genre, a.name as author, b.isbn, e.name as editorial, b.stock FROM book b INNER JOIN genre g ON g.id = b.genre INNER JOIN author a ON a.id = b.author INNER JOIN editorial e ON e.id = b.editorial WHERE b.name = ?`,
+  postNewBook: 'INSERT INTO book (name, genre, author, isbn, editorial, stock) VALUES (?, ?, ?, ?, ?, ?)',
+  updateBookStock: 'UPDATE book SET stock = ? WHERE id = ?'
 }
